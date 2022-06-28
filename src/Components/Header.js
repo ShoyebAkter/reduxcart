325e1fdcd7fd2ed66d120e432567eff090ea6b5d
Shoyeb180 <shoyebmohammad660@gmail.com>
2022-06-28 10:52:42 +0600

    router implemented

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import  Nav from 'react-bootstrap/Nav'
-import  Navbar from 'react-bootstrap/Navbar'
-import  Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Badge from '@mui/material/Badge';
+import Container from 'react-bootstrap/Container'
 
 class Header extends Component {
     render() {
         return (
-                <Navbar bg="dark" variant="dark">
-                    <Container>
-                        <Navbar.Brand href="#home">Add To Cart</Navbar.Brand>
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                        </Nav>
-                        <i class="fa-solid fa-cart-shopping text-white" style={{fontSize:25,cursor:"pointer"}}></i>
-                    </Container>
-                </Navbar>
-            
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Add To Cart</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                    </Nav>
+                    <Badge badgeContent={4} color="primary">
+                    <i class="fa-solid fa-cart-shopping text-white" style={{ fontSize: 25, cursor: "pointer" }}></i>
+                    </Badge>
+                </Container>
+            </Navbar>
+
         );
     }
 }
