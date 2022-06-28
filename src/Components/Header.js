3ed5222059d0cc598509dc8357182348acfc69ae
Shoyeb180 <shoyebmohammad660@gmail.com>
2022-06-28 15:01:37 +0600

    badge added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,27 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component,useState } from 'react';
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import Badge from '@mui/material/Badge';
 import Container from 'react-bootstrap/Container'
 import { NavLink } from 'react-router-dom';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
-class Header extends Component {
-    render() {
-        return (
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <NavLink to='/' className="text-decoration-none text-white">Add To Cart</NavLink>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                    </Nav>
-                    <Badge badgeContent={4} color="primary">
+const Header = () => {
+    const [anchorEl, setAnchorEl] = useState (null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    return (
+        <Navbar bg="dark" variant="dark" style={{ height: 60 }}>
+            <Container>
+                <NavLink to='/' className="text-decoration-none text-white">Add To Cart</NavLink>
+                <Nav className="me-auto">
+                    <NavLink to="#home" className="text-decoration-none text-white px-5">Home</NavLink>
+                </Nav>
+                <Badge badgeContent={4} color="primary"
+                    id="basic-button"
+                    aria-controls={open ? 'basic-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                    onClick={handleClick}
+                >
                     <i class="fa-solid fa-cart-shopping text-white" style={{ fontSize: 25, cursor: "pointer" }}></i>
-                    </Badge>
-                </Container>
-            </Navbar>
+                </Badge>
+            </Container>
+            <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'basic-button',
+                }}
+            >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
+        </Navbar>
 
-        );
-    }
+    );
 }
 
 export default Header;
